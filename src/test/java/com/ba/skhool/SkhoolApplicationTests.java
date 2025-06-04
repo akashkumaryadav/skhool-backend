53067cc1c74191f5cd5fe2bb7817db1b44358e29
akashkumaryadav <49105309+akashkumaryadav@users.noreply.github.com>
2025-06-04 20:45:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/test/java/com/ba/skhool/SkhoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ba.skhool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
